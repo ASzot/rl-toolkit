fa612bfb8fde6053c9ed63a0840c78c8d43b9d37
ASzot <me@andrewszot.com>
2020-08-15 20:09:10 -0400

    updated hab install script

--- envs/install_habitat.sh
+++ envs/install_habitat.sh
@@ -1,13 +1,13 @@
 # Habitat Sim
 conda create -n hab python=3.6 cmake=3.14.0
-conda activate hab
+source activate hab
 git clone --branch stable https://github.com/facebookresearch/habitat-sim.git ~/habitat-sim
 cd ~/habitat-sim
 pip install -r requirements.txt
-sudo apt-get install -y --no-install-recommends \
-     libjpeg-dev libglm-dev libgl1-mesa-glx libegl1-mesa-dev mesa-utils xorg-dev freeglut3-dev
-python setup.py install --with-cuda --headless
-# Install Habitat API
-git clone --branch stable https://github.com/facebookresearch/habitat-api.git ~/habitat-api
-cd ~/habitat-api
-pip install -e .
+#sudo apt-get install -y --no-install-recommends \
+#     libjpeg-dev libglm-dev libgl1-mesa-glx libegl1-mesa-dev mesa-utils xorg-dev freeglut3-dev
+python setup.py install --with-cuda --headless --bullet
+## Install Habitat API
+#git clone --branch stable https://github.com/facebookresearch/habitat-api.git ~/habitat-api
+#cd ~/habitat-api
+#pip install -e .
