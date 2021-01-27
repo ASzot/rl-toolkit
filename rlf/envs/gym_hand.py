e85147c3a4cbdb6c53a60cd7ac5e3ebf66f9314e
ASzot <me@andrewszot.com>
2021-01-27 12:01:50 -0800

    Can now easily concatenate the hand goal with the hand observation

--- rlf/envs/gym_hand.py
+++ rlf/envs/gym_hand.py
@@ -6,9 +6,35 @@ import gym
 
 
 class GymHandWrapper(gym.core.Wrapper):
+    def __init__(self, env, inc_goal):
+        super().__init__(env)
+        self.inc_goal = inc_goal
+        if self.inc_goal:
+            o_shape = env.observation_space
+            new_shape = o_shape['observation'].shape[0] + o_shape['desired_goal'].shape[0]
+            new_shape = (new_shape,)
+
+            self.observation_space.spaces['observation'] = rutils.reshape_obs_space(
+                    o_shape['observation'],
+                    new_shape)
+
+    def _trans_obs(self, obs):
+        if self.inc_goal:
+            obs['observation'] = np.concatenate([
+                obs['observation'], obs['desired_goal']
+                ])
+        return obs
+
+    def reset(self):
+        obs = super().reset()
+        obs = self._trans_obs(obs)
+        return obs
+
+
     def step(self,a):
         obs,reward,done,info = super().step(a)
-        info['ep_is_success'] = info['is_success']
+        obs = self._trans_obs(obs)
+        info['ep_found_goal'] = info['is_success']
         return obs,reward,done,info
 
 class GymHandInterface(EnvInterface):
@@ -19,10 +45,12 @@ class GymHandInterface(EnvInterface):
             reward_type = 'sparse'
 
         env = gym.make(env_id, reward_type=reward_type)
-        return GymHandWrapper(env)
+        return GymHandWrapper(env, self.args.hand_inc_goal)
 
     def get_add_args(self, parser):
         parser.add_argument('--hand-dense', action='store_true',
+                default=False)
+        parser.add_argument('--hand-inc-goal', action='store_true',
                 default=False)
 
 
