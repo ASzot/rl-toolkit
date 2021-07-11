d6301ff5ffe4b5ee65d6a785fba1e016e7ae231d
ASzot <me@andrewszot.com>
2021-07-10 23:23:39 -0400

    More tests

--- test/test_ppo.py
+++ test/test_ppo.py
@@ -6,7 +6,6 @@ from rlf.run_settings import RunSettings
 import pytest
 
 
-TEST_ENV = 'MountainCarContinuous-v0'
 NUM_ENV_SAMPLES = 1000
 NUM_STEPS = 100
 NUM_PROCS = 2
@@ -24,6 +23,12 @@ class PPORunSettings(RunSettings):
 
 
 
-def test_train():
+def test_cont_train():
+    TEST_ENV = 'Pendulum-v0'
+    run_settings = PPORunSettings(f"--prefix 'ppo-test' --use-proper-time-limits --linear-lr-decay True --lr 3e-4 --entropy-coef 0 --num-env-steps {NUM_ENV_SAMPLES} --num-mini-batch 32 --num-epochs 10 --num-steps {NUM_STEPS} --env-name {TEST_ENV} --eval-interval -1 --log-smooth-len 10 --save-interval -1 --num-processes {NUM_PROCS} --cuda False")
+    run_policy(run_settings)
+
+def test_disc_train():
+    TEST_ENV = 'Acrobot-v1'
     run_settings = PPORunSettings(f"--prefix 'ppo-test' --use-proper-time-limits --linear-lr-decay True --lr 3e-4 --entropy-coef 0 --num-env-steps {NUM_ENV_SAMPLES} --num-mini-batch 32 --num-epochs 10 --num-steps {NUM_STEPS} --env-name {TEST_ENV} --eval-interval -1 --log-smooth-len 10 --save-interval -1 --num-processes {NUM_PROCS} --cuda False")
     run_policy(run_settings)
